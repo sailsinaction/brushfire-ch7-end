c10c6d48d79c0e6364f48615dd1a26c2e39cc0de
Irl Nathan <irlnathan@gmail.com>
2016-03-06 16:20:12 -0600

    formatting

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,3 @@
-// angular.module('brushfire', ['toastr', 'compareTo'])
-//     .config(function($sceDelegateProvider) {
-//         $sceDelegateProvider.resourceUrlWhitelist([
-//             'self',
-//             '*://www.youtube.com/**'
-//         ]);
-//     });
-
 angular.module('brushfire', ['ngRoute', 'toastr', 'compareTo'])
   .config(['$sceDelegateProvider', function($sceDelegateProvider) {
     $sceDelegateProvider.resourceUrlWhitelist([
@@ -26,7 +18,6 @@ angular.module('brushfire', ['ngRoute', 'toastr', 'compareTo'])
 
   $routeProvider
 
-  // #/    (i.e. ng-view's "home" state)
     .when('/', {
     templateUrl: '/templates/home.html'
   })
@@ -43,103 +34,21 @@ angular.module('brushfire', ['ngRoute', 'toastr', 'compareTo'])
 
   .when('/videos', {
     templateUrl: '/templates/videos.html',
-    // If the current user is an admin, "redirect" (client-side) to `#/users`.
-    // Otherwise redirect to `#/profile`
     controller: 'videosPageController'
-      // controller: ['$scope', '$location', function($scope, $location) {
-      //   // if ($scope.me.isAdmin) {
-
-    //   //   // Instead of:
-    //   //   // window.location.hash = '#/users';
-
-    //   //   // We can do it the angular way:
-    //   //   // (to avoid a bunch of weird digest loop errors)
-    //     //$location.path('/users');
-    //   //   $location.replace();
-    //   //   return;
-    //   // }
-
-    //   // // Client-side redirect to `#/profile`
-    //   // $location.path('/profile');
-    //   // $location.replace();
-    //   // return;
-    // }]
   })
 
   .when('/profile/:id', {
     templateUrl: '/templates/profile.html',
-    // If the current user is an admin, "redirect" (client-side) to `#/users`.
-    // Otherwise redirect to `#/profile`
     controller: 'profilePageController'
-      // controller: ['$scope', '$location', function($scope, $location) {
-      //   // if ($scope.me.isAdmin) {
-
-    //   //   // Instead of:
-    //   //   // window.location.hash = '#/users';
-
-    //   //   // We can do it the angular way:
-    //   //   // (to avoid a bunch of weird digest loop errors)
-    //     //$location.path('/users');
-    //   //   $location.replace();
-    //   //   return;
-    //   // }
-
-    //   // // Client-side redirect to `#/profile`
-    //   // $location.path('/profile');
-    //   // $location.replace();
-    //   // return;
-    // }]
   })
 
   .when('/profile/edit/:id', {
     templateUrl: '/templates/edit-profile.html',
-    // If the current user is an admin, "redirect" (client-side) to `#/users`.
-    // Otherwise redirect to `#/profile`
     controller: 'editProfilePageController'
-      // controller: ['$scope', '$location', function($scope, $location) {
-      //   // if ($scope.me.isAdmin) {
-
-    //   //   // Instead of:
-    //   //   // window.location.hash = '#/users';
-
-    //   //   // We can do it the angular way:
-    //   //   // (to avoid a bunch of weird digest loop errors)
-    //     //$location.path('/users');
-    //   //   $location.replace();
-    //   //   return;
-    //   // }
-
-    //   // // Client-side redirect to `#/profile`
-    //   // $location.path('/profile');
-    //   // $location.replace();
-    //   // return;
-    // }]
   })
 
   .when('/restore', {
-    templateUrl: '/templates/restore.html',
-    // If the current user is an admin, "redirect" (client-side) to `#/users`.
-    // Otherwise redirect to `#/profile`
+    templateUrl: '/templates/restore-profile.html',
     controller: 'restorePageController'
-      // controller: ['$scope', '$location', function($scope, $location) {
-      //   // if ($scope.me.isAdmin) {
-
-    //   //   // Instead of:
-    //   //   // window.location.hash = '#/users';
-
-    //   //   // We can do it the angular way:
-    //   //   // (to avoid a bunch of weird digest loop errors)
-    //     //$location.path('/users');
-    //   //   $location.replace();
-    //   //   return;
-    //   // }
-
-    //   // // Client-side redirect to `#/profile`
-    //   // $location.path('/profile');
-    //   // $location.replace();
-    //   // return;
-    // }]
   });
-
-
 }]);
