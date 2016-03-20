5092ad594b61f0baa5b885ceb16d98c656341ce1
Irl Nathan <irlnathan@gmail.com>
2016-03-19 20:56:56 -0500

    finished out custom response

--- api/responses/alreadyInUse.js
+++ api/responses/alreadyInUse.js
@@ -4,13 +4,16 @@ module.exports = function alreadyInUse (err){
   // (since the arguments are up to us)
   var res = this.res;
 
-  console.log('res: ', res);
-
+  // A violation of the unique constraint for email
   if (err.invalidAttributes.email) {
     return res.send(409, 'Email address is already taken by another user, please try again.');
   }
 
+  // A violation of the unique constraint for username
   if (err.invalidAttributes.username) {
     return res.send(409, 'Username is already taken by another user, please try again.');
   }
+
+  // A catchall response
+  return res.send(500);
 };
