40445ff51189b2c4e2dcd8f254d3870fbcb46107
Irl Nathan <irlnathan@gmail.com>
2016-03-20 18:01:11 -0500

    chapter 7 changes

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -105,5 +105,207 @@ module.exports = {
         });
       }
     });
+  },
+
+  profile: function(req, res) {
+
+    User.findOne(req.param('id')).exec(function foundUser(err, user) {
+      
+      if (err) return res.negotiate(err);
+
+      // Handle no user being found
+      if (!user) return res.notFound();
+
+      // Build up user options
+      var options = {
+        email: user.email,
+        username: user.username,
+        gravatarURL: user.gravatarURL,
+        deleted: user.deleted,
+        admin: user.admin,
+        banned: user.banned,
+        id: user.id
+      };
+
+      // Return the user
+      return res.json(options);
+    });
+  },
+
+  delete: function(req, res) {
+
+    if (!req.param('id')){ 
+      return res.badRequest('id is a required parameter.');
+    }
+
+    User.destroy({ 
+      id: req.param('id')
+    }).exec(function (err, usersDestroyed){
+      if (err) return res.negotiate(err);
+      if (usersDestroyed.length === 0) {
+        return res.notFound();
+      }
+      return res.ok();
+    });
+  },
+
+  removeProfile: function(req, res) {
+    
+    if (!req.param('id')){
+      return res.badRequest('id is a required parameter.');
+    }
+
+    User.update({ 
+      id: req.param('id')
+    },{
+      deleted: true 
+    }, function(err, removedUser){
+
+      if (err) return res.negotiate(err);
+      if (removedUser.length === 0) {
+        return res.notFound();
+      }
+
+      return res.ok();
+    });
+  },
+
+  restoreProfile: function(req, res) {
+
+    User.findOne({
+      email: req.param('email')
+    }, function foundUser(err, user) {
+      if (err) return res.negotiate(err);
+      if (!user) return res.notFound();
+
+      Passwords.checkPassword({
+        passwordAttempt: req.param('password'),
+        encryptedPassword: user.encryptedPassword
+      }).exec({
+
+        error: function(err) {
+          return res.negotiate(err);
+        },
+
+        incorrect: function() {
+          return res.notFound();
+        },
+
+        success: function() {
+
+          User.update({
+            id: user.id
+          }, {
+            deleted: false
+          }).exec(function(err, updatedUser) {
+
+            return res.json(updatedUser);
+          });
+        }
+      });
+    });
+  },
+
+  restoreGravatarURL: function(req, res) {
+
+    try {
+     
+      var restoredGravatarURL = gravatarURL = Gravatar.getImageUrl({ 
+        emailAddress: req.param('email')              
+      }).execSync();
+
+      return res.json(restoredGravatarURL);
+
+    } catch (err) {
+      return res.serverError(err);
+    }
+  },
+
+  updateProfile: function(req, res) {
+
+    User.update({ 
+      id: req.param('id')
+    }, {
+      gravatarURL: req.param('gravatarURL')
+    }, function(err, updatedUser) {
+
+      if (err) return res.negotiate(err);
+
+      return res.json(updatedUser);
+
+    });
+  },
+
+  changePassword: function(req, res) {
+
+    if (_.isUndefined(req.param('password'))) {
+      return res.badRequest('A password is required!');
+    }
+
+    if (req.param('password').length < 6) {
+      return res.badRequest('Password must be at least 6 characters!');
+    }
+
+    Passwords.encryptPassword({
+      password: req.param('password'),
+    }).exec({
+      error: function(err) {
+        return res.serverError(err);
+      },
+      success: function(result) {
+
+        User.update({
+          id: req.param('id')
+        }, {
+          encryptedPassword: result
+        }).exec(function(err, updatedUser) {
+          if (err) {
+            return res.negotiate(err);
+          }
+          return res.json(updatedUser);
+        });
+      }
+    });
+  },
+
+  adminUsers: function(req, res) {
+
+    User.find().exec(function(err, users){ 
+
+      if (err) return res.negotiate(err);
+
+      return res.json(users);
+
+    });
+  },
+
+  updateAdmin: function(req, res) {
+
+    User.update(req.param('id'), {
+      admin: req.param('admin')
+    }).exec(function(err, update){
+
+     if (err) return res.negotiate(err);
+
+      res.ok();
+    });
+  },
+
+  updateBanned: function(req, res) {
+    User.update(req.param('id'), {
+      banned: req.param('banned')
+    }).exec(function(err, update){
+     if (err) return res.negotiate(err);
+      res.ok();
+    });
+  },
+
+  updateDeleted: function(req, res) {
+    User.update(req.param('id'), {
+      deleted: req.param('deleted')
+    }).exec(function(err, update){
+     if (err) return res.negotiate(err);
+      res.ok();
+    });
   }
 };
--- assets/js/controllers/signupPageController.js
+++ assets/js/controllers/signupPageController.js
@@ -22,10 +22,10 @@ angular.module('brushfire').controller('signupPageController', ['$scope', '$http
     .then(function onSuccess(sailsResponse){
 
       // Redirect to the profile page [This is after we have a profile page built]
-      // window.location = '#/profile/' + sailsResponse.data.id;
+      window.location = '#/profile/' + sailsResponse.data.id;
       
       // Redirect to the user blueprint record [This is before we have the profile page built]
-      window.location = '/user/' + sailsResponse.data.id;
+      // window.location = '/user/' + sailsResponse.data.id;
     })
     .catch(function onError(sailsResponse){
 
